49af884adc9abfc8f86a44047b368c88cda0628a
nazarii28 <73073550+nazarii28@users.noreply.github.com>
2021-03-09 20:18:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,37 @@ for (let anchor of anchors) {
     })
   })
 }
+
+const content = document.getElementById("count");
+document.addEventListener("scroll", function scroll(e) {
+
+var scrolled = document.scrollingElement.scrollTop;
+var position = content.offsetTop;
+
+
+if(scrolled > position - 700){
+startCountAnimation();
+document.removeEventListener('scroll', scroll)
+}
+
+});
+
+
+function startCountAnimation() {
+	let numbers = document.querySelectorAll('.out');
+	for(number of numbers) {
+	let end = +number.dataset.max;
+	let step = +number.dataset.step;
+	count(number, end, step);
+}
+}
+
+
+function count(number, end, step) {
+let interval = setInterval(function() {
+	number.innerHTML = +number.innerHTML + step;
+	if(+number.innerHTML == end) {
+		clearInterval(interval)
+	}
+}, 15);
+}
